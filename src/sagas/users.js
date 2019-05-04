ab49424996860cf144b1d9e23c50434431e6b936
Carol Enache <enache.carol@gmail.com>
2019-05-04 16:00:14 +0100

    handle error in try/catch

--- src/sagas/users.js
+++ src/sagas/users.js
@@ -9,7 +9,9 @@ function* getUsers(){
             items: result.data.data
         }));
     }catch(e){
-
+        yield put(actions.userError({
+            error: 'An error ocurred when trying to get the user'
+        }))
     }
 }
 
@@ -22,7 +24,9 @@ function* createUser(action){
     yield call(api.createUser, {firstName: action.payload.firstName, lastName: action.payload.lastName});
     yield call(getUsers);
    }catch(e){
-
+       yield put(actions.userError({
+           error: 'An error ocurred when trying to create the user'
+       }));
    }
 }
 
@@ -35,7 +39,9 @@ function* deleteUser(userId) {
         yield call(api.deleteUser, userId);
         yield call(getUsers);
     }catch(e){
-
+        yield put(actions.userError({
+            error: 'An error ocurred when trying to delete the user'
+        }))
     }
 }
 
